e4a8017a1dee31f09014639b9aa59eeae2389aa4
stanislavlyalin <lyalinstas@gmail.com>
2022-09-30 18:18:53 +0300

    Added `login` endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/login?<email>&<password>")]
+fn login(email: &str, password: &str) -> String {
+    format!("{} {}", email, password)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api/v1", routes![login])
 }
