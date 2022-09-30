e8766f9c2b44a44d534a294cee3386c1fe70ac23
stanislavlyalin <lyalinstas@gmail.com>
2022-10-01 00:39:28 +0300

    Implemented `login` endpoint

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,40 @@
 extern crate rocket;
 
 use rocket::serde::json::Json;
+use std::fs::File;
+use std::io::{self, BufRead, Error};
+use std::path::Path;
 
 fn hash(email: String, password: String) -> String {
-    let digest = md5::compute(email + password.as_str() + "my_awesome_service");
-    return format!("{:x}", digest);
+    let salt = "my_awesome_service";
+    let digest = md5::compute(email + password.as_str() + salt);
+    format!("{:x}", digest)
+}
+
+fn is_user_registered(token: &str) -> bool {
+    fn file_contains(filepath: &str, token: &str) -> Result<bool, Error> {
+        if Path::new(filepath).exists() {
+            let file = File::open(filepath)?;
+            let lines = io::BufReader::new(file).lines();
+            return Ok(lines.filter(|l| l.as_ref().unwrap().eq(token)).count() > 0);
+        }
+        Ok(false)
+    }
+
+    return match file_contains("user_hashes.txt", token) {
+        Ok(r) => r,
+        Err(_) => false,
+    };
 }
 
 #[get("/login?<email>&<password>")]
 fn login(email: &str, password: &str) -> Json<String> {
     let token = hash(email.to_string(), password.to_string());
-    println!("{}", token);
-    Json(token)
+
+    if is_user_registered(token.as_str()) {
+        return Json(token);
+    }
+    Json("".to_string())
 }
 
 #[launch]
