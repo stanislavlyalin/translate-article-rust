73479972a30d4add5a48070a8d193f4915c4a1a6
stanislavlyalin <lyalinstas@gmail.com>
2022-09-30 21:55:25 +0300

    Added md5-hash calculation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 #[macro_use]
 extern crate rocket;
 
+fn hash(email: String, password: String) -> String {
+    let digest = md5::compute(email + password.as_str() + "my_awesome_service");
+    return format!("{:x}", digest);
+}
+
 #[get("/login?<email>&<password>")]
 fn login(email: &str, password: &str) -> String {
+    println!("{}", hash(email.to_string(), password.to_string()));
     format!("{} {}", email, password)
 }
 
