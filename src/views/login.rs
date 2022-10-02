ec581d2c6d26a6d80329c663597ea15267353de7
stanislavlyalin <lyalinstas@gmail.com>
2022-10-02 21:37:41 +0300

    Used ref semantic in functions

--- src/views/login.rs
+++ src/views/login.rs
@@ -3,10 +3,10 @@ use rocket::serde::json::Json;
 use crate::utils::auth::{hash, is_user_registered};
 
 #[get("/login?<email>&<password>")]
-pub fn login(email: &str, password: &str) -> Json<String> {
-    let token = hash(email.to_string(), password.to_string());
+pub fn login(email: String, password: String) -> Json<String> {
+    let token = hash(&email, &password);
 
-    if is_user_registered(token.as_str()) {
+    if is_user_registered(&token) {
         return Json(token);
     }
     Json("".to_string())
